8a31dbe39f9ea83b4313bf8a1fb20e64ffb3cc80
Thiengend <thien100305@gmail.com>
2026-09-25 21:39:54 +0700

    Fix static images

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
